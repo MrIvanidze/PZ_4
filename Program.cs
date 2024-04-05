74286c46e2699a1500ef07e197a42df2debcd36b
MrIvanidze <tkachukivan1309@gmail.com>
2024-04-05 20:41:25 +0300

    ASCII

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
-﻿namespace PZ4_2
+﻿namespace PZ_4_N4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            char[] alphabet = new char[26];
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                alphabet[i] = (char)(i + 'a');
+            }
+            foreach (char i in alphabet)
+            {
+                Console.WriteLine($"Letter - {i}. In the ASCII table it's: {(int)i}");
+            }
         }
     }
 }
