2d9fd7076cd6c4b0ea2b39044bef0c86cb68b0b0
MrIvanidze <tkachukivan1309@gmail.com>
2024-04-05 20:21:48 +0300

    Elements

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-﻿namespace PZ4_2
+﻿using System.Xml.Linq;
+
+namespace PZ_4_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] elements = new int[50];
+            int count = 0;
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                count += 2;
+                elements[i] = count;
+            }
+
+            foreach (int element in elements)
+            {
+                Console.WriteLine($"Element: {element}");
+            }
+
+
         }
     }
 }
