9ec9601100a8a1cce4ebc6e5d87370bcde9ac2b7
MrIvanidze <tkachukivan1309@gmail.com>
2024-04-05 20:14:59 +0300

    Random

--- Program.cs
+++ Program.cs
@@ -1,10 +1,37 @@
-﻿namespace PZ4_2
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace PZ_4_N2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] RandomArray = new int[36];
+            Random random = new Random();
+            int[] PairArray = { };
+
+            for (int i = 0; i < RandomArray.Length; i++)
+            {
+                RandomArray[i] = random.Next(0, 50);
+
+                if (RandomArray[i] % 2 == 0)
+                {
+                    PairArray = PairArray.Append(RandomArray[i]).ToArray();
+                }
+            }
+
+            Console.WriteLine($"Arrays include {PairArray.Length} pair numbers.");
+            Console.Write("It's: ");
+
+            for (int i = 0; i < PairArray.Length - 1; i++)
+            {
+                Console.Write($"{PairArray[i]}, ");
+            }
+
+            Console.Write($"{PairArray[^1]}.");
+
         }
     }
 }
