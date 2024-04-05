1a7e13ad7007ca1782bd09b5929fc031c2d48bd1
MrIvanidze <tkachukivan1309@gmail.com>
2024-04-05 20:32:10 +0300

    Length

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿namespace PZ4_2
+﻿using System.Diagnostics.Metrics;
+
+namespace PZ_4_N3_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("You want to know the length of your text? No problem! \n" +
+                "Enter your text here: ");
+            string text = Console.ReadLine();
+
+            Console.WriteLine($"Text “{text}” include: {text.Length} symbols");
         }
     }
 }
